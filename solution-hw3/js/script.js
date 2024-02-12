b9efb808701d8ee1c8ffa66928a8de7082721dcb
pkbae03 <143964064+pkbae03@users.noreply.github.com>
2024-02-12 14:35:48 -0500

    added js

---
+++ solution-hw3/js/script.js
@@ -0,0 +1,19 @@
+function glazingChange(element) {
+    // get value of selected glazing option
+    const priceChange = element.value;
+    const glaze = document.getElementById(glaze).glaze;
+
+    for i 
+
+    
+  // add your code to do update the price ...
+  }
+  
+(basePrice + glazePrice) * size
+  function sizeChange(element) {
+    // get value of selected glazing option
+    const priceChange = element.value;
+    
+    
+  // add your code to do update the price ...
+  }
--- solution-hw3/random-element.js
+++
@@ -1,7 +0,0 @@
-function glazingChange(element) {
-    // get value of selected glazing option
-    const priceChange = element.value;
-    
-  // add your code to do update the price ...
-  }
-  
